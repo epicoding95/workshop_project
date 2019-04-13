938fc607894a36cef8b6b3172a089ccff7bc2eee
your_name <your_email>
2019-04-13 13:13:31 -0700

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// var myFunction = function(dinosaur) {
+//   alert(dinosaur);
+// }
+
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
